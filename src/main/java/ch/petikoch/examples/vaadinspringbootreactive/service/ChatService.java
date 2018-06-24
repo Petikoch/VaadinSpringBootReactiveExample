d8638dbee26fd57b0eb30bee0361c0591ff51eed
Peti Koch <petikoch@gmail.com>
2018-06-24 20:55:22 +0200

    use ReplayProcessor instead of EmitterProcessor

--- src/main/java/ch/petikoch/examples/vaadinspringbootreactive/service/ChatService.java
+++ src/main/java/ch/petikoch/examples/vaadinspringbootreactive/service/ChatService.java
@@ -1,8 +1,8 @@
 package ch.petikoch.examples.vaadinspringbootreactive.service;
 
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.ReplayProcessor;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -12,7 +12,7 @@ public class ChatService {
 
 	private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
 
-	private final EmitterProcessor<String> chatMessages = EmitterProcessor.create();
+	private final ReplayProcessor<String> chatMessages = ReplayProcessor.create();
 
 	public Flux<String> chatMessages() {
 		return chatMessages.serialize();
